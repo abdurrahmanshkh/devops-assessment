d72d7ef9750a2e3acac0ae2f4acea1442dd6fb7e
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2026-03-18 04:34:18 +0530

    Refactor security group egress rules: remove old block and implement specific rule for HTTPS to S3

--- terraform/security.tf
+++ terraform/security.tf
@@ -11,15 +11,23 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow only HTTPS outbound (instead of all ports)
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # REMOVE the old egress block completely
 
   tags = {
     Name = "devops-assessment-sg"
   }
+}
+
+data "aws_prefix_list" "s3" {
+  name = "com.amazonaws.${var.aws_region}.s3"
+}
+
+resource "aws_security_group_rule" "egress_s3" {
+  type              = "egress"
+  security_group_id = aws_security_group.web_sg.id
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  prefix_list_ids   = [data.aws_prefix_list.s3.id]
+  description       = "Allow HTTPS only to S3"
 }
