e3e434316816c825378398598cc12473453375c3
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2026-03-18 04:29:02 +0530

    Refactor security group rules: remove SSH ingress and restrict outbound to HTTPS only

--- terraform/security.tf
+++ terraform/security.tf
@@ -3,16 +3,6 @@ resource "aws_security_group" "web_sg" {
   description = "Security group for web application"
   vpc_id      = aws_vpc.main_vpc.id
 
-  # REMEDIATED VULNERABILITY: SSH restricted to a specific trusted IP
-  ingress {
-    description = "SSH from Admin IP only"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["182.48.218.175/32"]
-  }
-
-  # Open port 3000 for our Node.js app (Open to the world is acceptable for a public web app)
   ingress {
     description = "App Port"
     from_port   = 3000
@@ -21,11 +11,11 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow only HTTPS outbound (instead of all ports)
   egress {
-    # trivy:ignore:AVD-AWS-0104 (Accepted Risk: Needs outbound internet to download Docker)
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
